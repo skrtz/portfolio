8a61d8961bc6630e97d52029feb9e91edf85d70c
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-23 21:30:55 -0400

    update contact info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="contact">
+      <h1>Contact Me</h1>
         <p>stephenjkurtz@gmail.com</p>
-        <p>resume</p>
+        <a href="https://drive.google.com/file/d/1l1Mfh4Grfzn5fL8EXQsTvEVB60j4m8AB/view?usp=sharing" target="_blank">Click to view my resume</a>
     </div>
   );
 }
