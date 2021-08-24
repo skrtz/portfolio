cd09a5bf4ad128f2b945f846eb284b21ba36d0a7
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-23 21:30:10 -0400

    update bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,8 +10,9 @@ export default function About() {
       <p >
         My name is Stephen Kurtz and I am a front-end developer from Philadelphia, PA. Along with my coding
         skills I leverage my many years of experience in the service industry and marketing by building user
-        frendly & responsive web applications. Outside of VS Code however I am either making music or
-        finding new ways to destroy my kitchen. For general inquiries please feel free to reach out to me!
+        frendly & responsive web applications. I have my certificate in MERN stack web developement from 
+        UPenn Coding Bootcamp and seek to continue to build my knowledge of new technologies. For general 
+        inquiries please feel free to reach out to me!
       </p>
 
     </div>
