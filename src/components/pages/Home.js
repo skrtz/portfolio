9d64dd531b90aa5e653d148559557dcf9b40f482
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-27 18:48:27 -0400

    update homepage styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,17 @@ function Home() {
     return (
         <div className="title-page">
             <div className="title">
-                <h1>Stephen</h1>
-                <h1>Kurtz</h1>
-                <h2>Front</h2>
-                <h2>End</h2>
-                <h2>Developer</h2>
+                <div>
+                    <div className="nameTitle">
+                        <h1>Stephen</h1>
+                        <h1>Kurtz</h1>
+                    </div>
+                    <div className="subTitle">
+                        <h2>Front</h2>
+                        <h2>End</h2>
+                        <h2>Developer</h2>
+                    </div>
+                </div>
             </div>
         </div>
     )
