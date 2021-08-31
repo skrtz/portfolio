dac5aa4e1e8d8b9fcdd63776cb1bb94891fb37b5
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-31 18:01:58 -0400

    set landing page to HOME

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 import '../App.css';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('HOME');
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'HOME') {
