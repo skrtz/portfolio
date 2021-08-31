1984074b370d5058032dfa205c6d5e6cdd967fed
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-31 16:07:45 -0400

    add hambuger navigation functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,32 @@
-function Nav({ currentPage, handlePageChange }) {
-    const pages = ['Home', 'About', 'Projects', 'Contact'];
+import React, { useState } from "react";
+
+function Nav({ currentPage, handlePageChange, }) {
+    const pages = ['HOME', 'ABOUT', 'PROJECTS', 'CONTACT'];
     const navLinks = pages.map(page => {
+
         return (
-            <a href={'/#' + page.toLocaleLowerCase()} onClick={() => handlePageChange(page)} >
+            <a href={'/#' + page.toLocaleLowerCase()} onClick={() => handlePageChange(page)}>
                 {page}
             </a>
         )
     });
-    return <nav className="nav" >{navLinks}</nav>;
+    let [menuOpen, setMenuOpen] = useState(false);
+    const handleClick = () => {
+        if (!menuOpen) {
+            menuOpen = true;
+        } else {
+            menuOpen = false;
+        }
+        setMenuOpen(!menuOpen);
+        console.log(menuOpen);
+    }
+    return (
+        <div className="nav-container">
+            <div className={`menu-btn ${menuOpen ? " open" : ""}`} onClick={handleClick}>
+                <div className="menu-btn__burger"></div>
+            </div>
+            <nav className={`nav ${menuOpen ? " open" : ""}`}>{navLinks}</nav>
+        </div>
+    );
 }
 export default Nav;
