6b8af68c691fd8218a1dc841a087ad5f84fd0418
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-09-08 12:20:36 -0400

    udpate project thumbnail layout

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,7 +2,8 @@ import weather from '../../assets/weather.png';
 import dnd from '../../assets/DnD.png';
 import scheduler from '../../assets/scheduler.png';
 import { FaGithub } from 'react-icons/fa';
-import { AiOutlineCode } from 'react-icons/ai'
+import { AiOutlineRocket } from 'react-icons/ai';
+import { AiOutlineCode } from 'react-icons/ai';
 
 export default function Projects() {
     return (
@@ -14,24 +15,23 @@ export default function Projects() {
                         <h2>Weather App</h2>
                         {/* <p>Weather Dashboard provides a five day forecast for any city as well as the current weather by using open weather API to retrieve the data.</p> */}
                         <div className="project-links">
-                            <a className="site-link">deployed site</a>
+                            <a className="site-link" target="_blank" rel="noreferrer" href="https://skrtz.github.io/weather-dashboard/">deployed site</a>
                             <div className="icon-links">
-                                <a> <FaGithub /> </a>
+                                <a className="rocket" target="_blank" rel="noreferrer" href="https://skrtz.github.io/weather-dashboard/"> <AiOutlineRocket /> </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* <div className="code"> <AiOutlineCode size={70} />
-                </div> */}
                 <div className="img-thumb">
                     <img alt="DnD app" src={dnd}></img>
                     <div className="img-caption">
                         <h2>MERN App</h2>
                         {/* <p>D&D Creations provides users with a way to keep track of characters they build as well as view other characters in the group. It uses MongoDB and GraphQL to handle database functionality, and uses React to display the front end UI.</p> */}
                         <div className="project-links">
-                            <a className="site-link">deployed site</a>
+                            <a className="site-link" target="_blank" rel="noreferrer" href="https://dnd-character-create.herokuapp.com/" >deployed site</a>
                             <div className="icon-links">
-                                <a> <FaGithub /> </a>
+                                <a className="rocket" target="_blank" rel="noreferrer" href="https://dnd-character-create.herokuapp.com/" ><AiOutlineRocket /></a>
+
                             </div>
                         </div>
                     </div>
@@ -42,11 +42,19 @@ export default function Projects() {
                         <h2>Schedule App</h2>
                         {/* <p>Workday Scheduler keeps track of a users daily tasks and uses MomentJS to provide a color coded UI.</p> */}
                         <div className="project-links">
-                            <a className="site-link">deployed site</a>
+                            <a className="site-link" target="_blank" rel="noreferrer" href="https://skrtz.github.io/workday-scheduler/">deployed site</a>
                             <div className="icon-links">
-                                <a> <FaGithub /> </a>
+                                <a className="rocket" target="_blank" rel="noreferrer" href="https://skrtz.github.io/workday-scheduler/"><AiOutlineRocket /></a>
+                                
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="info-container">
+                    <div className="code">
+                        <a className="github" target="_blank" rel="noreferrer" href="https://github.com/skrtz/weather-dashboard"> <FaGithub size={30} /> </a>
+                        <a className="github" target="_blank" rel="noreferrer" href="https://github.com/skrtz/DnD-Character-Builder"> <FaGithub size={30}/> </a>
+                        <a className="github" target="_blank" rel="noreferrer" href="https://github.com/skrtz/workday-scheduler"> <FaGithub size={30}/> </a>
                     </div>
                 </div>
             </div>
