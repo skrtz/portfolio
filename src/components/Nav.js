413a1f709542a53347b77009bd37e3baa2608064
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-09-09 00:14:41 -0400

    update react router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
-// function Nav({ currentPage, handlePageChange, }) {
-//     const pages = ['HOME', 'ABOUT', 'PROJECTS', 'CONTACT'];
-//     const navLinks = pages.map(page => {
-//         return (
-//             <a href={'/#' + page.toLocaleLowerCase()} onClick={() => {setMenuOpen(false);handlePageChange(page)}}>
-//                 {page}
-//             </a>
-//         )
-//     });
-//     let [menuOpen, setMenuOpen] = useState(false);
-//     const handleClick = () => {
-//         if (!menuOpen) {
-//             menuOpen = true;
-//         } else {
-//             menuOpen = false;
-//         }
-//         setMenuOpen(!menuOpen);
-//         console.log(menuOpen);
-//     }
-//     return (
-//         <div className="nav-container">
-//             <div className={`menu-btn ${menuOpen ? " open" : ""}`} onClick={handleClick}>
-//                 <div className="menu-btn__burger"></div>
-//             </div>
-//             <nav className={`nav ${menuOpen ? " open" : ""}`}>{navLinks}</nav>
-//         </div>
-//     );
-// }
-// export default Nav;
-
-
 function Nav() {
     const pages = ['home', 'about', 'projects', 'contact'];
     const navLinks = pages.map(page => {
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-// import Burger from './Burger'
+import React from 'react';
 import NavTabs from './Nav';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,30 +8,9 @@ import '../App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function PortfolioContainer() {
-  // const [currentPage, setCurrentPage] = useState('HOME');
-  // // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  // const renderPage = () => {
-  //   if (currentPage === 'HOME') {
-  //     return <Home />;
-  //   }
-  //   if (currentPage === 'ABOUT') {
-  //     return <About />;
-  //   }
-  //   if (currentPage === 'PROJECTS') {
-  //     return <Projects />;
-  //   }
-  //   return <Contact />;
-  // };
-
-  // const handlePageChange = (page) => {
-  //   setCurrentPage(page);
-  // };
-
   return (
     <Router>
       <div className="main">
-        {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()} */}
         <NavTabs />
         <Switch>
           <Route path="/" exact component={Home} />
