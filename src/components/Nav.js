85768312bc1ccceaa991bf5515da032e4c3a9cad
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-09-08 23:02:21 -0400

    implement react router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,57 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
-function Nav({ currentPage, handlePageChange, }) {
-    const pages = ['HOME', 'ABOUT', 'PROJECTS', 'CONTACT'];
+// function Nav({ currentPage, handlePageChange, }) {
+//     const pages = ['HOME', 'ABOUT', 'PROJECTS', 'CONTACT'];
+//     const navLinks = pages.map(page => {
+//         return (
+//             <a href={'/#' + page.toLocaleLowerCase()} onClick={() => {setMenuOpen(false);handlePageChange(page)}}>
+//                 {page}
+//             </a>
+//         )
+//     });
+//     let [menuOpen, setMenuOpen] = useState(false);
+//     const handleClick = () => {
+//         if (!menuOpen) {
+//             menuOpen = true;
+//         } else {
+//             menuOpen = false;
+//         }
+//         setMenuOpen(!menuOpen);
+//         console.log(menuOpen);
+//     }
+//     return (
+//         <div className="nav-container">
+//             <div className={`menu-btn ${menuOpen ? " open" : ""}`} onClick={handleClick}>
+//                 <div className="menu-btn__burger"></div>
+//             </div>
+//             <nav className={`nav ${menuOpen ? " open" : ""}`}>{navLinks}</nav>
+//         </div>
+//     );
+// }
+// export default Nav;
+
+
+function Nav() {
+    const pages = ['home', 'about', 'projects', 'contact'];
     const navLinks = pages.map(page => {
-        return (
-            <a href={'/#' + page.toLocaleLowerCase()} onClick={() => {setMenuOpen(false);handlePageChange(page)}}>
-                {page}
-            </a>
-        )
+        if ( page  === 'home') {
+            return (
+                <Link to='/'>
+                    <li onClick={() => { setMenuOpen(false) }}>
+                        {page}
+                    </li>
+                </Link>
+            )
+        } else {
+            return (
+                <Link to={page}>
+                    <li onClick={() => { setMenuOpen(false) }}>
+                        {page}
+                    </li>
+                </Link>
+            )
+        }
     });
     let [menuOpen, setMenuOpen] = useState(false);
     const handleClick = () => {
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,32 +6,41 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import '../App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('HOME');
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === 'HOME') {
-      return <Home />;
-    }
-    if (currentPage === 'ABOUT') {
-      return <About />;
-    }
-    if (currentPage === 'PROJECTS') {
-      return <Projects />;
-    }
-    return <Contact />;
-  };
+  // const [currentPage, setCurrentPage] = useState('HOME');
+  // // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // const renderPage = () => {
+  //   if (currentPage === 'HOME') {
+  //     return <Home />;
+  //   }
+  //   if (currentPage === 'ABOUT') {
+  //     return <About />;
+  //   }
+  //   if (currentPage === 'PROJECTS') {
+  //     return <Projects />;
+  //   }
+  //   return <Contact />;
+  // };
 
-  const handlePageChange = (page) => {
-      setCurrentPage(page);
-    };
+  // const handlePageChange = (page) => {
+  //   setCurrentPage(page);
+  // };
 
   return (
-    <div className="main">
-      {/* <Burger /> */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
-    </div>
+    <Router>
+      <div className="main">
+        {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()} */}
+        <NavTabs />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
