d1687d15bd4f67902e8b5d62fb5f2b25c1ceb536
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-09-13 12:49:14 -0400

    update resume file link

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Resume from '../../assets/resume_2021.pdf'
+import Resume from '../../assets/resume.pdf'
 
 export default function Contact() {
   return (
     <div className="contact">
       <h1>Contact Me</h1>
-      <p>stephenjkurtz@gmail.com</p>
       <a href={Resume} download="kurtz-resume">
-        Click to view my resume
+        Click to view resume and contact info
       </a>
     </div>
   );
