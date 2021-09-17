ace894e89ce022fc63ae5f47ee2f08b1086ea485
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-09-17 12:29:27 -0400

    update contact page style

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Resume from '../../assets/resume.pdf'
+import Resume from '../../assets/resume.pdf';
+import { FiThumbsUp } from 'react-icons/fi';
 
 export default function Contact() {
   return (
+    <a href={Resume} download="kurtz-resume">
     <div className="contact">
       <h1>Contact Me</h1>
       <a href={Resume} download="kurtz-resume">
         Click to view resume and contact info
       </a>
+      <FiThumbsUp size={30}/>
     </div>
+  </a>
+
   );
 }
